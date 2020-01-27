6f89ee8b63beadc8d7bf57255f1f6e36582bc1bc
shpaker <shpaker@gmail.com>
2020-01-27 19:31:03 +0300

    2.0.0 -> 2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wifi_qrcode",
-    version="2.0.0",
+    version="2.1.0",
     author="Aleksandr Shpak",
     author_email="shpaker@gmail.com",
     description="Generate WiFi Access QR Codes",
